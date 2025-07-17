e566b2d1392909fdd8f220f84b3923b226c5dcc8
emlznv <emlznv@gmail.com>
2025-07-17 13:43:40 +0300

    Remove opencv

--- src/screens/ImageProcessing/ImageProcessing.tsx
+++ src/screens/ImageProcessing/ImageProcessing.tsx
@@ -1,9 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+// import { useEffect } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+// import { NativeModules } from 'react-native';
+// const { NotanProcessor } = NativeModules;
 
-const ImageProcessing = () => {
+const ImageProcessing = ({ route }: { route: any }) => {
+  const { imageUri } = route.params;
+
+  // useEffect(() => {
+  //   const processImage = async () => {
+  //     const tones = 3; // for example
+  //     const resultPath = await NotanProcessor.applyTonalSegmentation(
+  //       imageUri,
+  //       tones,
+  //     );
+  //     console.log('Segmented image path:', resultPath);
+  //   };
+
+  //   processImage();
+  // }, [imageUri]);
+
   return (
     <View style={styles.container}>
-      <Text>Image Processing</Text>
+      {imageUri ? (
+        <Image
+          source={{ uri: imageUri }}
+          style={{ width: 300, height: 300, resizeMode: 'contain' }}
+        />
+      ) : (
+        <Text>No image provided</Text>
+      )}
     </View>
   );
 };
